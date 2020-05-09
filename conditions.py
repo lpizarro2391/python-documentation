e1c9e305a6261e57a4099cc65e2bf2f4f7ba80a9
lpizarro <lpizarro2391@github.com>
2020-05-08 22:52:48 -0600

    el operador not se cumple si la variable esta en falso. si ambas variables estan en true, no imprime mensaje

--- conditions.py
+++ conditions.py
@@ -1,6 +1,6 @@
-has_high_income = True
 has_good_credit = True
+has_criminal_record = True
 
-if has_good_credit or has_high_income:
+if has_good_credit and not has_criminal_record:
     print("Eligle for loan")
 
