7dec49814936fa4812186643acf2fe01e22c2244
lpizarro <lpizarro2391@github.com>
2020-05-09 00:36:56 -0600

    card game,  while condition para que se indique que ya el carro esta iniciado , y no repita el mensaje todaslas veces que el carro ha iniciado, , enonces se necesita saber si el carro esta el carro iniciado o no? true o false!  se agrega variable started. se evalua si esta el carro inidicado, se entra de una vez que si esta iniciado se manda el else y si vuelve a escribir se manda en if started

--- conditions.py
+++ conditions.py
@@ -1,11 +1,27 @@
-secret_number = 9
-guess_count= 0
-guess_limit = 3
-while guess_count < guess_limit :
-    guess= int(input('Guess: '))
-    guess_count += 1
-    if guess == secret_number:
-        print('You wom')
+command = ""
+started = False
+while True:
+    command = input(">").lower()
+    if command =="start":
+        if started:
+            print("Car is already started!")
+        else:
+            started = True
+            print("Car started....")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("car stopped.")
+    elif command =="help":
+        print('''
+star - to star the car
+stop - to stop the car
+quit - to quite
+help - to help
+         ''')
+    elif command =="quit":
         break
-else: 
-    print('Sorry you failed')
+    else:
+        print("Sorry , I dont understand")
