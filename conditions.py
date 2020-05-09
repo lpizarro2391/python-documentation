3e38d526d89e521929c7636642137bc1ed65ed7a
lpizarro <lpizarro2391@github.com>
2020-05-09 17:52:27 -0600

    exceptions try, zero divisionError:

--- conditions.py
+++ conditions.py
@@ -1,5 +1,9 @@
 try : 
     age = int(input('Age: '))
+    income = 20000
+    risk = income / age
     print(age)
+except ZeroDivisionError:
+    print('Edad no puede ser cero')
 except ValueError:
     print('Invalid value')
