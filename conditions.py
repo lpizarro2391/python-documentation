330b705029fab841d34e4652fd77a40b7de47bcf
lpizarro <lpizarro2391@github.com>
2020-05-09 07:04:56 -0600

    for ( iteracion) , in,  se imprime en una linea para abajo

--- conditions.py
+++ conditions.py
@@ -1,27 +1,2 @@
-command = ""
-started = False
-while True:
-    command = input(">").lower()
-    if command =="start":
-        if started:
-            print("Car is already started!")
-        else:
-            started = True
-            print("Car started....")
-    elif command == "stop":
-        if not started:
-            print("Car is already stopped!")
-        else:
-            started = False
-            print("car stopped.")
-    elif command =="help":
-        print('''
-star - to star the car
-stop - to stop the car
-quit - to quite
-help - to help
-         ''')
-    elif command =="quit":
-        break
-    else:
-        print("Sorry , I dont understand")
+for  item in 'python':
+    print(item)
