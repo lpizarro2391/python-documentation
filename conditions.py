f628336a76fb9efa4430bab559aa674d8ca3ef9e
lpizarro <lpizarro2391@github.com>
2020-05-09 07:56:14 -0600

    listas encontrar la variable con el numero maximo, hay que definir en otra variable max. se necesita iterar en la lista hsta encontrar el maximo, revisar es mas grande 3 que 6? , despues es mas grande 2 que 6?

--- conditions.py
+++ conditions.py
@@ -1,4 +1,6 @@
-names = ['Jonh', 'bob', 'Mohs', 'Sarah', 'Maria']
-names[0] = 'Jon'
-print(names)
-
+numbers = [3, 6, 2, 8, 4, 10]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
