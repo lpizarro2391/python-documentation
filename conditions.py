ff193ca514ceb16cbf9d0189356b06bd193f1231
lpizarro <lpizarro2391@github.com>
2020-05-09 07:44:29 -0600

    intern loop para resolver el mismo problema anterior. se define una variable output vacia para que represente las variables que estan dentro del rango y se imprime. numbers = [5, 3, 5, 2, 2] for x_count in numbers:     output = ''     for count in range(x_count):         output +='x'     print(output)

--- conditions.py
+++ conditions.py
@@ -1,4 +1,7 @@
-number = [5, 2, 5, 2, 2,]
-for x_count in number:
-    print('x'* x_count)
+numbers = [5, 3, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output +='x'
+    print(output)
 
