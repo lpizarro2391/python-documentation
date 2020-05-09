be719a803445a5ee33c9fea0dee6581bd3162b94
lpizarro <lpizarro2391@github.com>
2020-05-08 22:37:30 -0600

    else quiere decir que es falso la condicion

--- conditions.py
+++ conditions.py
@@ -3,4 +3,7 @@ is_hot = False
 if is_hot:
     print("It´s a hot day")
     print("Drink plenty of water")
+else:
+    print("It's a cold day")
+    print("wear warm clothes")
 print("Enjoy you day")
