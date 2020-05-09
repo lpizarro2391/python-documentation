6404fc15f6226d80341fbe2e24786570ba81f808
lpizarro <lpizarro2391@github.com>
2020-05-09 14:57:14 -0600

    diccionarios, split poner emoticon , el signo ">" quiere decir que el usuario debe ingresar algun mensaje , con split separamos lo que ingrese el usuario en palabras individuales  separados por ' '

--- conditions.py
+++ conditions.py
@@ -1,13 +1,5 @@
-phone = input("phone: ")
-digits_mapping = {
-"1": "uno",
-"2": "dos" , 
-"3": "tres",
-"4": "cuatro"
-}
-output = ""
-for ch in phone:
-    output += digits_mapping.get(ch, "!") + " "
-    print(output)
+message = input(">")
+words = message.split(' ')
+print(words)
 
 
