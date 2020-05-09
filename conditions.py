cd8a678aa5ea392c6c301a19a4a69d584db0af62
lpizarro <lpizarro2391@github.com>
2020-05-09 15:15:36 -0600

    dicctionario , emoticones , con control + command + espacio se ponen los emoticones

--- conditions.py
+++ conditions.py
@@ -1,5 +1,12 @@
 message = input(">")
 words = message.split(' ')
-print(words)
+emojis = {
+    ":)": "😀",
+    ":(": "😟"
+}
+output = ""
+for word in words:
+    output += emojis.get(word, word) + " "
+print(output)
 
 
