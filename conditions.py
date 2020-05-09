8899f333c0d28884e7121ba39e048958df2294f9
lpizarro <lpizarro2391@github.com>
2020-05-09 08:02:06 -0600

    matriz 2 dimentional list.  es una lista dentro de otra lista , tipo matriz, cada matriz se representa una fila para cada linea  con parentesis cuadrados , se llama cada fila de la matriz con parentesis cuadrados y se inicia otra lista nueva en cada fila,

--- conditions.py
+++ conditions.py
@@ -1,6 +1,7 @@
-numbers = [3, 6, 2, 8, 4, 10]
-max = numbers[0]
-for number in numbers:
-    if number > max:
-        max = number
-print(max)
+matriz = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+print(matriz[0][1])
