14d8285a452b93c5ff4cf3c8260d206504170c03
lpizarro <lpizarro2391@github.com>
2020-05-09 17:46:55 -0600

    exceptions , try except se pone la excepcion de que si aparece VallueError se debe poner el mensaje Invalid value..

--- conditions.py
+++ conditions.py
@@ -1,14 +1,5 @@
-def emoji_converter(message):
-    words = message.split(' ')
-    emojis = {
-    ":)": "😀",
-    ":(": "😟"
-    }
-    output = ""
-    for word in words:
-        output += emojis.get(word, word) + " "
-    return output
-
-
-message = input(">")
-print(emoji_converter(message))
+try : 
+    age = int(input('Age: '))
+    print(age)
+except ValueError:
+    print('Invalid value')
