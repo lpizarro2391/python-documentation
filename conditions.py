823fa6e758d0868b81a5120a640ddbe2ad56b25b
lpizarro <lpizarro2391@github.com>
2020-05-09 15:43:40 -0600

    funciones,  si eliminamos el nombre de la funcion ya no va a funcionar, porque en la funcion dice que tiene que haber un nombre. las funciones reciben informacion, y se debe suplir esa informacion, para renombrar funciones se puede usar esa opcion y se renombran todas las funciones.

--- conditions.py
+++ conditions.py
@@ -1,10 +1,10 @@
-def saludo_usuario(name):
-    print(f'Hi {name} !')
+def saludo_usuario(first_name,last_name):
+    print(f'Hi {first_name}{last_name} !')
     print('bienvenidos a bordo')
 
 
 print("start")
-saludo_usuario("John")
-saludo_usuario("Mary")
+saludo_usuario("John", "Perez")
+saludo_usuario("Mary", "Castro")
 print("finish")
 
