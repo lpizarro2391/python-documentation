b9a502e2a63544338441e604209a2fd10afb085d
lpizarro <lpizarro2391@github.com>
2020-05-09 07:47:19 -0600

    listas, names = ['Jonh', 'bob', 'Mohs', 'Sarah', 'Maria'] print(names)  imprime una fila con los nombres

--- conditions.py
+++ conditions.py
@@ -1,7 +1,3 @@
-numbers = [5, 3, 5, 2, 2]
-for x_count in numbers:
-    output = ''
-    for count in range(x_count):
-        output +='x'
-    print(output)
+names = ['Jonh', 'bob', 'Mohs', 'Sarah', 'Maria']
+print(names)
 
