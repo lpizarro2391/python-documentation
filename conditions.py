62892c6822ffdf1ace241345d1aeb6f03e0d477c
lpizarro <lpizarro2391@github.com>
2020-05-09 18:22:22 -0600

    class contructor def __init__es para construir un objeto

--- conditions.py
+++ conditions.py
@@ -1,17 +1,15 @@
 class Point:
+    def __init__(self, x, y):
+        self.x = x 
+        self.y = y
+
     def move(self):
         print("move")
     
     def draw(self):
         print("draw")
 
-point1 = Point()
-point1.x = 10
-point1.y = 20
-print(point1.x)
-point1.draw()
 
-point2 = Point()
-point2.x = 1
-print(point2.x)
-
+point = Point(10, 20)
+point.x = 10
+print(point.x)
